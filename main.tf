e763d5b9233b294cab90cfe04cba47bf7a39492a
kayode <aiffone@gmail.com>
2021-10-01 15:10:29 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "eaglesbyte"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "github-actions"
     }
   }
 }
